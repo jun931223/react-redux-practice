7a5d04f98aaeafafdb1be0faa275f574e085db61
Yi JunHee <jun931223@gmail.com>
2021-05-29 12:35:57 +0900

    TIL: begin to study redux

--- my-react-app/src/store.js
+++ my-react-app/src/store.js
@@ -1,29 +1,29 @@
-import {createStore} from 'redux'
+import { createStore } from "redux";
 // import {reducer} from './reducers'
 
 let initialState = {
-    displayNum:0
-}
+  displayNum: 0,
+};
 
-let reducer = (state=initialState, action) => {
-    switch (action.type) {
-        case 'INCREMENT' :
-            return Object.assign({}, state, {displayNum: state.displayNum+action.addNum})
-
+let reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return Object.assign({}, state, {
+        displayNum: state.displayNum + action.addNum,
+      });
 
     default:
-        return state
-    }
-}
+      return state;
+  }
+};
 
-const store = createStore(reducer
-// chrome에서 개발자 도구에 Redux 의 동작을 확인 할 수 있게 인자로 추가
-, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+const store = createStore(
+  reducer
+  // chrome에서 개발자 도구에 Redux 의 동작을 확인 할 수 있게 인자로 추가
+  // , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-
-
-export default store
+export default store;
 
 /*
 store는 state = {}
@@ -33,4 +33,3 @@ methods
 3. subscribe()
 4. replaceReducer()
 */
-
